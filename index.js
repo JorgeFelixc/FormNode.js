833649bdcac2510a7ae4d40a4398a1a74f187110
Jorge Felix Cazarez <jorgefelix97@gmail.com>
2019-05-28 19:01:53 -0600

    all users

--- index.js
+++ index.js
@@ -7,24 +7,6 @@ const express = require("express"),
     shell = require("shelljs");
 
     //abriendo power shell
-var  spawn = require("child_process").spawn,child;
-if(process.platform === "win32"){
-  child = spawn("powershell.exe",["C:\\Users\\dell\\Desktop\\Proyectos\\Servers\\NodeForm\\createUser.ps1"])
-  child.stdout.on("data",(data) =>{
-    console.log("Powershell data: " + data);
-  })
-  child.stderr.on("data",(data) =>{
-    console.log("Powershell error: " + data);
-  })
-  child.on("exit",() =>{
-    console.log("Powershell finished. ");
-  })
-  
-  vbs = spawn("cscript.exe", ["C:\\Users\\dell\\Desktop\\Proyectos\\Servers\\NodeForm\\CrearCuentaMail.vbs"]);
-  console.log(`stderr: ${vbs.stderr.toString()}`)
-  console.log(`stdout: ${vbs.stdout.toString()}`)
-  console.log(`status: ${vbs.status}`)
-}
 
 
 // EJECUTAR UBUNTU SHELL
@@ -51,6 +33,40 @@ route.get('/', (req,res) =>{
 route.post('/server',(req,res) =>{
   res.sendFile(path.join(__dirname+'/post.html'));
 })
+
+route.post('/register', (req,res) =>{
+  
+  var s = req.body;
+  console.log(s);
+  registerUsers(s);
+  res.send(s);
+});
+
+function registerUsers({user, password}){
+  // console.log(`this is a string ${user} , and ${password} , ANDDDDD ${email}`);
+  var  spawn = require("child_process").spawn,child;
+  var str = `C:\\Users\\dell\\Desktop\\Proyectos\\Servers\\NodeForm\\createUser.ps1 -arg1 ${user} -arg2 ${password} ` 
+  if(process.platform === "win32"){
+    child = spawn("powershell.exe",[str])
+    child.stdout.on("data",(data) =>{
+      console.log("Powershell data: " + data);
+    })
+    child.stderr.on("data",(data) =>{
+      console.log("Powershell error: " + data);
+    })
+    child.on("exit",() =>{
+      console.log("Powershell finished. ");
+    })
+    var strVb = `C:\\Users\\dell\\Desktop\\Proyectos\\Servers\\NodeForm\\CrearCuentaMail.vbs -arg1 ${user} -arg2 ${password}`
+    vbs = spawn("cscript.exe", [strVb]);
+    console.log(`stderr: ${vbs.stderr.toString()}`)
+    console.log(`stdout: ${vbs.stdout.toString()}`)
+    console.log(`status: ${vbs.status}`)
+  }
+
+}
+
+
 
 app.use('/',route);
 app.listen(3000,'localhost');
